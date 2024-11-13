9ebfc34234f0e8077d215aab0a2972d96bcb52d1
Chrisw131 <walkerchris0523@gamil.com>
2024-11-13 15:02:57 -0500

    Lab303.9.1

---
+++ src/EncapNotes.java
@@ -0,0 +1,9 @@
+// Encapsulation - hiding the class variable from other classes and giving access to them only through methods (setters and getters).
+
+// Encapsulation in Java means binding the data (variables) with the code (methods – setters and getters). The below diagram depicts Encapsulation in Java.
+
+
+public class EncapNotes {
+
+	
+}
